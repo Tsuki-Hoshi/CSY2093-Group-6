81b0cec0e8b6129174aed21fcb1f814424d89100
Warren Browne <warrenbrowne413@gmail.com>
2025-11-28 15:45:30 +0000

    Object type definied along with multidimensional VARRAY

--- CSY2093_Group_6.sql
+++ CSY2093_Group_6.sql
@@ -1,10 +1,20 @@
 -- DROP all the table/object
-
-
+DROP TABLE facilities_varray_type;
+DROP TYPE facilities_type;
 
 -- CREATE all the table/object
+CREATE OR REPLACE TYPE facilities_type AS OBJECT (
+    name        VARCHAR2(40),
+    description VARCHAR2(255),
+    capacity    NUMBER(5),
+    opening_time DATE,
+    closing_time DATE,
+    entry_price NUMBER(7,2)
+);
+/
 
-
+CREATE TYPE facilities_varray_type AS VARRAY(100) OF facilities_type;
+/
 
 -- INSERT example
 
