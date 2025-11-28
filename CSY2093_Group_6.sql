64c412623ba7d03a1f97a01471edbd39ffbe153e
Warren Browne <warrenbrowne413@gmail.com>
2025-11-28 16:35:27 +0000

    SHITTY MC SHIT ON THE MAIN

--- CSY2093_Group_6.sql
+++ CSY2093_Group_6.sql
@@ -3,15 +3,6 @@ DROP TABLE facilities_varray_type;
 DROP TYPE facilities_type;
 
 -- CREATE all the table/object
-CREATE TABLE hotels (
-    hotel_id NUMBER(6),
-    name VARCHAR2(40),
-    description VARCHAR2(255),
-    rating CHAR(1),
-    contact_no VARCHAR2(15),
-    capacity NUMBER(4),
-    addresses REF address_type SCOPE IS addresses
-);
 
 CREATE OR REPLACE TYPE facilities_type AS OBJECT (
     name        VARCHAR2(40),
@@ -26,11 +17,21 @@ CREATE OR REPLACE TYPE facilities_type AS OBJECT (
 CREATE TYPE facilities_varray_type AS VARRAY(100) OF facilities_type;
 /
 
+CREATE TABLE hotels (
+    hotel_id NUMBER(6),
+    name VARCHAR2(40),
+    description VARCHAR2(255),
+    rating CHAR(1),
+    contact_no VARCHAR2(15),
+    capacity NUMBER(4),
+    addresses REF address_type SCOPE IS addresses,
+    facilities facilities_varray_type
+);
+
 -- CONSTRAINTS
 ALTER TABLE hotels
 ADD CONSTRAINT pk_hotels
 PRIMARY KEY (hotel_id);
-
 -- INSERT example
 
 
