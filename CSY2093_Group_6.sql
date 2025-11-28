8e941ea8b7121ff98b7c3268da2dd94718c2fa3c
Tsuki-Hoshi <junyo.ishizaka@gmail.com>
2025-11-28 16:55:34 +0000

    Added all assigned Varray_Type and Table

--- CSY2093_Group_6.sql
+++ CSY2093_Group_6.sql
@@ -33,6 +33,9 @@ CREATE OR REPLACE TYPE address_type AS OBJECT (
 );
 /
 
+CREATE TYPE duration_varray_type AS VARRAY(2) OF DATE;
+/
+
 -- VARRAY TYPES
     
 CREATE TYPE facilities_varray_type AS VARRAY(100) OF facilities_type;
@@ -62,6 +65,25 @@ CREATE TABLE travellers (
     dob             DATE,
     address         address_type
 );
+
+CREATE TABLE trip_categories(
+trip_category_id NUMBER(6),
+duration duration_varray_type,
+minimum_age NUMBER(3),
+genre VARCHAR2(40),
+name VARCHAR2(40),
+description VARCHAR2(255));
+
+CREATE TABLE trips(
+trip_id NUMBER(6),
+trip_category_id NUMBER(6),
+hotel_id NUMBER(6),
+name VARCHAR2(40),
+description VARCHAR2(255),
+start_date DATE,
+end_date DATE,
+activities activity_table_type)
+NESTED TABLE activities STORE AS activity_table;
 
 -- -- CONSTRAINTS
 
