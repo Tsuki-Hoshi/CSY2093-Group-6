01632cea5f60fb9fddb266862907dce56f85e8cd
Yameen <sm1.yameen@gmail.com>
2025-11-28 15:46:45 +0000

    Creating my set of tables and types
    Added SQL commands to create types and tables for travellers and addresses.

--- CSY2093_Group_6.sql
+++ CSY2093_Group_6.sql
@@ -1,19 +1,53 @@
--- DROP all the table/object
+-- Yameen's Work Here
+
+-- -- DROP all the table/object
+
+-- RELATIONAL TABLES
+DROP TABLE travellers;
+
+-- OBJECT TABLES
+DROP TABLE addresses;
+
+-- TYPES
+DROP TYPE address_type;
 
 
 
--- CREATE all the table/object
+-- -- CREATE all the Types / Objects / Tables
+
+-- TYPES
+
+CREATE OR REPLACE TYPE address_type AS OBJECT (
+    street      VARCHAR2(40),
+    city        VARCHAR2(40),
+    country     VARCHAR2(40)
+);
+/
+
+-- OBJECT TABLES
+
+CREATE TABLE addresses OF address_type;
+
+
+-- RELATIONAL TABLES
+
+CREATE TABLE travellers (
+    traveller_id    NUMBER(6),
+    firstname       VARCHAR2(40),
+    surname         VARCHAR2(40),
+    dob             DATE,
+    address         REF OF address_type SCOPE IS addresses
+);
 
 
 
--- INSERT example
+-- -- INSERT example
 
 
 
--- QUERY example
+-- -- QUERY example
 
 
 
 
 
--- yam test wowie
