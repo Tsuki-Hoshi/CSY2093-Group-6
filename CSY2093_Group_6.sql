a87f4f19b804f993c0f2e7ee2fa0ad690204a60d
Tsuki-Hoshi <junyo.ishizaka@gmail.com>
2025-12-12 16:29:21 +0000

    insert into trip_categories

--- CSY2093_Group_6.sql
+++ CSY2093_Group_6.sql
@@ -134,12 +134,70 @@ tickets
 
 -- -- INSERTS
 
+INSERT INTO trip_categories
+VALUES (
+    200001,
+    duration_varray_type(
+        DATE ('12-DEC-2025'),
+        DATE ('31-DEC-2025')
+    ),
+    18,
+    'CHRISTMAS',
+    'SNOWFLAKE',
+    'MULTIPLE ACTIVITIES WITH SNOW LIKE SKIING AND BUILDING A SNOWMAN'
+);
 
+INSERT INTO trip_categories
+VALUES (
+    200002,
+    duration_varray_type(
+        DATE('01-JAN-2026'),
+        DATE('10-JAN-2026')
+    ),
+    12,
+    'NEW YEAR',
+    'FIREWORK FEST',
+    'CELEBRATION EVENTS WITH FIREWORK SHOWS AND STREET FOOD'
+);
 
+INSERT INTO trip_categories
+VALUES (
+    200003,
+    duration_varray_type(
+        DATE('14-FEB-2026'),
+        DATE('20-FEB-2026')
+    ),
+    16,
+    'VALENTINE',
+    'HEARTWARM',
+    'ROMANTIC ACTIVITIES LIKE COUPLE COOKING AND SKY LANTERNS'
+);
 
+INSERT INTO trip_categories
+VALUES (
+    200004,
+    duration_varray_type(
+        DATE('01-APR-2026'),
+        DATE('05-APR-2026')
+    ),
+    10,
+    'SPRING',
+    'BLOOMFEST',
+    'FLOWER GARDEN TOURS AND PICNIC ACTIVITIES IN FULL SPRING BLOOM'
+);
 
-
-
+INSERT INTO trip_categories
+VALUES (
+    200005,
+    duration_varray_type(
+        DATE('15-JUL-2026'),
+        DATE('25-JUL-2026')
+    ),
+    15,
+    'SUMMER',
+    'SUNBURST',
+    'BEACH ACTIVITIES LIKE SWIMMING, VOLLEYBALL, AND BOAT RIDES'
+);
 
 
 -- -- QUERIES
