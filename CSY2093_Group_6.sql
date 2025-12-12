3277ad6047ff098e59d8506f6c51f1fd8eb333a0
Yameen <sm1.yameen@gmail.com>
2025-12-12 17:36:14 +0000

    my shit type shit

--- CSY2093_Group_6.sql
+++ CSY2093_Group_6.sql
@@ -1,20 +1,41 @@
 
--- -- DROP all the table/object
+-- my copy of all the work just as a local backup
+
+-- -- DROP ALL TABLES / OBJECTS / TYPES
 
 -- RELATIONAL TABLES
+
+DROP TABLE hotels;
 DROP TABLE travellers;
 
 -- OBJECT TABLES
+
 DROP TABLE addresses;
 
--- TYPES
+-- NESTED TABLES
+
+DROP TYPE activity_table_type;
+
+-- OBJECT TYPES
+
+DROP TYPE facilities_varray_type;
+DROP TYPE facilities_type;
 DROP TYPE address_type;
+DROP TYPE activity_type;
 
-
-
--- -- CREATE all the Types / Objects / Tables
+-- -- CREATE ALL TYPES / OBJECTS / TABLES
 
 -- TYPES
+
+CREATE OR REPLACE TYPE facilities_type AS OBJECT (
+    name        VARCHAR2(40),
+    description VARCHAR2(255),
+    capacity    NUMBER(5),
+    opening_time DATE,
+    closing_time DATE,
+    entry_price NUMBER(7,2)
+);
+/
 
 CREATE OR REPLACE TYPE address_type AS OBJECT (
     street      VARCHAR2(40),
@@ -23,12 +44,46 @@ CREATE OR REPLACE TYPE address_type AS OBJECT (
 );
 /
 
+CREATE OR REPLACE TYPE activity_type AS OBJECT (
+    name            VARCHAR2(40),
+    description     VARCHAR2(255),
+    activity_count  NUMBER(5),
+    duration        NUMBER(5),
+    capacity        NUMBER(5),
+    genre           VARCHAR2(40)
+);
+/
+
+-- VARRAY TYPES
+    
+CREATE TYPE facilities_varray_type AS VARRAY(100) OF facilities_type;
+/
+
+CREATE TYPE duration_varray_type AS VARRAY(2) OF DATE;
+/
+
+
 -- OBJECT TABLES
 
 CREATE TABLE addresses OF address_type;
 
+-- NESTED TABLES
+
+CREATE TYPE activity_table_type AS TABLE OF activity_type;
+/
 
 -- RELATIONAL TABLES
+    
+CREATE TABLE hotels (
+    hotel_id NUMBER(6),
+    name VARCHAR2(40),
+    description VARCHAR2(255),
+    rating CHAR(1),
+    contact_no VARCHAR2(15),
+    capacity NUMBER(4),
+    addresses REF address_type SCOPE IS addresses,
+    facilities facilities_varray_type
+);
 
 CREATE TABLE travellers (
     traveller_id    NUMBER(6),
@@ -38,25 +93,197 @@ CREATE TABLE travellers (
     address         address_type
 );
 
+                                            -- add username maybe for functions on travellers
 
+CREATE TABLE trip_categories(
+trip_category_id NUMBER(6),
+duration duration_varray_type,
+minimum_age NUMBER(3),
+genre VARCHAR2(40),
+name VARCHAR2(40),
+description VARCHAR2(255));
 
+CREATE TABLE trips(
+trip_id NUMBER(6),
+trip_category_id NUMBER(6),
+hotel_id NUMBER(6),
+name VARCHAR2(40),
+description VARCHAR2(255),
+start_date DATE,
+end_date DATE,
+activities activity_table_type)
+NESTED TABLE activities STORE AS activity_table;
 
 -- -- CONSTRAINTS
+
+-- PRIMARY KEYS
+
+ALTER TABLE hotels
+ADD CONSTRAINT pk_hotels
+PRIMARY KEY (hotel_id);
 
 ALTER TABLE travellers
 ADD CONSTRAINT pk_travellers
 PRIMARY KEY (traveller_id);
 
+-- FOREIGN KEYS
+
+                                                -- -- VERIFY IF WE NEED TO DROP CONSTRAINTS FOR CAROLE
 
 
+trips 2
+tickets
 
 -- -- INSERTS
 
+-- TRAVELLERS
+
+INSERT INTO travellers
+VALUES (100001, 'JUNYO', 'ISHIZAKA ANTARASEN', '31-MAY-2005', address_type('10 FUNKY WAY', 'PHUKET', 'THAILAND'));
+
+INSERT INTO travellers
+VALUES (100002, 'NATNAEL', 'SINTAYEHU', '15-FEB-2006', address_type(' 16 LEAGUE STREET', 'JIMMA', 'ETHIOPIA'));
+
+INSERT INTO travellers
+VALUES (100003, 'SYED YAMEEN', 'MAHDI', '14-MAR-2006', address_type('49 COOL ALLEYWAY', 'SYLHET', 'BANGLADESH'));
+
+INSERT INTO travellers
+VALUES (100004, 'WARREN', 'BROWNE', '24-APR-2003', address_type('4 SILENT ROAD', 'NORTHAMPTON', 'UK'));
+
+INSERT INTO travellers
+VALUES (100005, 'HUGO', 'VEIL', '06-JAN-1800', address_type('13 SERRIN LAND', 'UTOPIA', 'SERITH'));
+
+-- TRIP CATEGORIES
+
+INSERT INTO trip_categories
+VALUES (
+    200001,
+    duration_varray_type(
+        DATE ('12-DEC-2025'),
+        DATE ('31-DEC-2025')
+    ),
+    18,
+    'CHRISTMAS',
+    'SNOWFLAKE',
+    'MULTIPLE ACTIVITIES WITH SNOW LIKE SKIING AND BUILDING A SNOWMAN'
+);
+
+INSERT INTO trip_categories
+VALUES (
+    200002,
+    duration_varray_type(
+        DATE('01-JAN-2026'),
+        DATE('10-JAN-2026')
+    ),
+    12,
+    'NEW YEAR',
+    'FIREWORK FEST',
+    'CELEBRATION EVENTS WITH FIREWORK SHOWS AND STREET FOOD'
+);
+
+INSERT INTO trip_categories
+VALUES (
+    200003,
+    duration_varray_type(
+        DATE('14-FEB-2026'),
+        DATE('20-FEB-2026')
+    ),
+    16,
+    'VALENTINE',
+    'HEARTWARM',
+    'ROMANTIC ACTIVITIES LIKE COUPLE COOKING AND SKY LANTERNS'
+);
+
+INSERT INTO trip_categories
+VALUES (
+    200004,
+    duration_varray_type(
+        DATE('01-APR-2026'),
+        DATE('05-APR-2026')
+    ),
+    10,
+    'SPRING',
+    'BLOOMFEST',
+    'FLOWER GARDEN TOURS AND PICNIC ACTIVITIES IN FULL SPRING BLOOM'
+);
+
+INSERT INTO trip_categories
+VALUES (
+    200005,
+    duration_varray_type(
+        DATE('15-JUL-2026'),
+        DATE('25-JUL-2026')
+    ),
+    15,
+    'SUMMER',
+    'SUNBURST',
+    'BEACH ACTIVITIES LIKE SWIMMING, VOLLEYBALL, AND BOAT RIDES'
+);
+
+-- HOTELS
+
+
+
+-- TRIPS
+
+INSERT INTO trips
+VALUES (400001, 200001, 300001, 'ALPINE BREAK',
+    'ENJOY OUTDOOR ACTIVITIES',
+    '17-JUN-2025', '25-AUG-2025',
+    activity_table_type(
+        activity_type('MOUNTAIN HIKE', 'GUIDED GROUP HIKE THROUGH SCENIC MOUNTAIN TRAILS', 12, 4, 20, 'OUTDOOR'),
+        activity_type('LAKESIDE PICNIC', 'RELAXING PICNIC BY THE LAKE WITH PROVIDED MEALS', 8, 15, 'CULINARY'),
+        activity_type('ROCK CLIMBING SESSION', 'BEGINNER-FRIENDLY CLIMBING ON NATURAL SURFACES', 3, 3, 12, 'ADVENTURE'))
+);
+
+INSERT INTO trips
+VALUES (400002, 200002, 300002, 'CITY EXPLORER WEEKEND',
+    'ENJOY THE CITY THIS WEEKEND',
+    '10-DEC-2016', '11-DEC-2016',
+    activity_table_type(
+        activity_type('HISTORIC MUSEUM TOUR', 'GUIDED WALK THROUGH MAJOR HISTORICAL MUSEUM HIGHLIGHTS', 5, 2, 30, 'CULTURAL'),
+        activity_type('STREET FOOD WALK', 'VISIT SEVERAL POPULAR FOOD STALLS AND LEARN ABOUT LOCAL CUISINE', 7, 2, 20, 'CULINARY'),
+        activity_type('RIVER CRUISE', 'CALM SIGHTSEEING CRUISE ALONG THE CITY''S RIVER', 1, 1, 50, 'LEISURE'))                                          -- check this
+);
+
+INSERT INTO trips
+VALUES (400003, 200003, 300003, 'DESERT ADVENTURE TOUR',
+    'EXPLORE THE VAST DESERTS',
+    '22-DEC-2025', '28-DEC-2025',
+    activity_table_type(
+        activity_type('SAND DUNE SAFARI', 'OFF-ROAD VEHICLE RIDE ACROSS SAND DUNES', 4, 3, 15, 'ADVENTURE'),
+        activity_type('CAMEL TREK', 'TRADITIONAL CAMEL RIDE ACROSS THE DESERT', 1, 2, 18, 'OUTDOOR'),
+        activity_type('STARGAZING NIGHT CAMP', 'NIGHTTIME DESERT CAMP FOCUSED ON ASTRONOMY AND CONSTELLATIONS', 1, 5, 25, 'EDUCATIONAL'))
+);
+
+INSERT INTO trips
+VALUES (400004, 200004, 300004, 'FESTIVAL AND CULTURE TOUR'
+    'EXPERIENCE THE FESTIVAL AND THE CULTURE OF THE CITY',
+    '29-DEC-2025', '04-JAN-2026',
+    activity_table_type(
+        activity_type('MUSIC FESTIVAL ENTRY', 'FULL-DAY ADMISSION TO MAJOR MUSIC FESTIVAL', 1, 10, 100, 'ENTERTAINMENT'),
+        activity_type('LOCAL CRAFTS WORKSHOP', 'HANDS-ON WORKSHOP MAKING TRADITIONAL CRAFT ITEMS', 4, 2, 20, 'CULTURAL'),
+        activity_type('PHOTOGRAPHY WALK', 'GUIDED PHOTO WALK THROUGH COLOURFUL FESTIVAL AREAS', 6, 3, 25, 'ARTISTIC'))
+);
+
+INSERT INTO trips
+VALUES (400005, 200005, 300005, 'ISLAND RESORT RETREAT',
+    'CHECK OUT THIS SWAGGER ISLAND AND THESE ACTIVITIES',
+    '09-AUG-2027', '15-AUG-2027',
+    activity_table_type(
+        activity_type('SNORKELING TOUR', 'GUIDED SNORKELING SESSION IN SHALLOW CORAL REEFS', 3, 2, 20, 'WATER'),
+        activity_type('BEACH YOGA SESSION', 'MORNING GUIDED YOGA ON THE BEACH', 1, 1, 15, 'WELLNESS'),
+        activity_type('SUNSET BOAT RIDE', 'EVENING BOAT RIDE WITH SCENIC OCEAN VIEWS', 1, 1, 30, 'LEISURE'))
+);
 
 
 -- -- QUERIES
 
 
 
+/*
+NOTES
 
+EXCEPTION HANDLING
 
+*/
