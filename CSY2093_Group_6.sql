f6fcf683176323242472993bfaf0dd10c69f9927
Warren Browne <warrenbrowne413@gmail.com>
2025-12-19 16:19:51 +0000

    Inserted hotels

--- CSY2093_Group_6.sql
+++ CSY2093_Group_6.sql
@@ -255,55 +255,54 @@ VALUES (
     'BEACH ACTIVITIES LIKE SWIMMING, VOLLEYBALL, AND BOAT RIDES'
 );
 
-ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MON/YYYY';
-
 -- HOTELS
 
-                                                -- -- VERIFY THIS WITH CAROLE
+ALTER SESSION SET NLS_DATE_FORMAT = "HH24:MI";
 
-INSERT INTO hotels (hotel_id, name, description, rating, contact_no, capacity, addresses, facilities)
-VALUES (300001, 'Park Plaza London Westminster Bridge', 'Modern hotel on Londons South Bank, opposite Big Ben, offering stylish rooms, spa, pool, dining, and easy access to major attractions.', 'B', '03334006112', 2000, 
-(REF(a) FROM addresses a WHERE street = '200 Westminster Bridge Rd'), 
-facility_varray_type(
+INSERT INTO hotels (hotel_id, name, description, rating, contact_no, capacity, facilities, addresses)
+SELECT 300001, 'Park Plaza London Westminster Bridge',
+    'Modern hotel on Londons South Bank, opposite Big Ben, offering stylish rooms, spa, pool, dining, and easy access to major attractions.', 'B', '03334006112', 2000,
+facilities_varray_type(
     facilities_type('Restaurant', 'On-site dining facility serving breakfast, lunch and dinner', 200, '09:00', '22:00', 0.00),
     facilities_type('Bar', 'Hotel bar offering drinks and light snacks', 150, '13:00', '23:00', 0.00),
-    facilities_type('Spa', 'Wellness facility offering treatments and relaxation', 50, '09:00', '20:00', 66.00))
-    );
+    facilities_type('Spa', 'Wellness facility offering treatments and relaxation', 50, '09:00', '20:00', 66.00)),
+    REF(a) FROM addresses a WHERE street = '200 Westminster Bridge Rd';
     
-INSERT INTO hotels (hotel_id, name, description, rating, contact_no, capacity, addresses, facilities)
-VALUES (300002, 'President Hotel', 'President Hotel Camden provides comfortable, affordable accommodation in central London, close to Camden Town and major transport links—ideal for tourists, students, and business travellers.', 'C', '02078378844', 1000, 
-(REF(a) FROM addresses a WHERE street = '56-60 Guilford Street'), 
-facility_varray_type(
+INSERT INTO hotels (hotel_id, name, description, rating, contact_no, capacity, facilities, addresses)
+SELECT 300002, 'President Hotel', 'President Hotel Camden provides comfortable, affordable accommodation in central London, close to Camden Town and major transport links—ideal for tourists, students, and business travellers.', 'C', '02078378844', 1000,  
+facilities_varray_type(
     facilities_type('Restaurant', 'On-site restaurant serving breakfast and evening meals', 200, '07:00', '22:00', 0.00),
     facilities_type('Bar', 'Hotel bar offering drinks and light snacks', 150, '12:00', '23:00', 0.00),
     facilities_type('Conference Room', 'Meeting and conference facilities for events and business use', 300, '08:00', '21:00', 0.00),
-    facilities_type('Lounge', 'Guest lounge area for relaxation and informal meetings', 100, '00:00', '23:59', 0.00))
-    );
+    facilities_type('Lounge', 'Guest lounge area for relaxation and informal meetings', 100, '00:00', '23:59', 0.00)),
+    REF(a) FROM addresses a WHERE street = '56-60 Guilford Street';
 
 
-INSERT INTO hotel (hotel_id, name, rating, contact_no, capacity, addresses, facilities, description)
-VALUES(300003, 'Plaza Timber', 'B', '07306036151', 2050, REF(a) FROM addresses WHERE a.street= '9 Junction Road', 'Ensuite room with free food and access to the Pool bar',
-facility_varry_type(
-    facility_type('Pool', 'On-site pool where guests have fun whenever they want', 150, '10.30', '22.30', 0.00),
-    facility_type('Room Service', 'Workers clean everyones room when they are not in their rooms', 0, '09.00','08.00', 0.00),
-    facility_type('Kids club3', 'This is for children only to have fun and do activities', 100, '12.00', '18.00', 0.00)
-));
+INSERT INTO hotels (hotel_id, name, rating, contact_no, capacity, description, facilities, addresses)
+SELECT 300003, 'Plaza Timber', 'B', '07306036151', 2050, 'Ensuite room with free food and access to the Pool bar',
+facilities_varray_type(
+    facilities_type('Pool', 'On-site pool where guests have fun whenever they want', 150, '10:30', '22:30', 0.00),
+    facilities_type('Room Service', 'Workers clean everyones room when they are not in their rooms', 0, '09:00','08:00', 0.00),
+    facilities_type('Kids club', 'This is for children only to have fun and do activities', 100, '12:00', '18:00', 0.00)),
+    REF(a) FROM addresses a WHERE street= '9 Junction Road';
 
-INSERT INTO hotel (hotel_id, name, rating, contact_no, capacity, addresses, facilities, description)
-VALUES(300004, 'Hotel Oak Bridge', 'C', '073890321171', 1, REF(a) FROM addresses WHERE a.street= '73 St. Michaels Road', 'Ensuite with a free mini fridge and access to the Bar'
-facility_varry_type(
-    facility_type('Wifi', 'Access Wifi to keep memories on social media', 50, '00.00', '00.00', 0.00),
-    facility_type('Bar', 'Access to the bar so adults can drink beverages or soft drinks', 50, '05.00', '01.00', 0.00),
-    facility_type('Pool bar', 'Access to beverages while swimming with the family', 100, '10.30','22.30', 0.00)
-));
+INSERT INTO hotels (hotel_id, name, rating, contact_no, capacity, description, facilities, addresses)
+SELECT 300004, 'Hotel Oak Bridge', 'C', '073890321171', 1, 'Ensuite with a free mini fridge and access to the Bar',
+facilities_varray_type(
+    facilities_type('Wifi', 'Access Wifi to keep memories on social media', 50, '00:00', '00:00', 0.00),
+    facilities_type('Bar', 'Access to the bar so adults can drink beverages or soft drinks', 50, '00:00', '01:00', 0.00),
+    facilities_type('Pool bar', 'Access to beverages while swimming with the family', 100, '10:30','22:30', 0.00)),
+REF(a) FROM addresses a WHERE street= '73 St. Michaels Road';
 
-INSERT INTO hotel (hotel_id, name, rating, contact_no, capacity, addresses, facilities, description)
-VALUES(300005, 'Premier Hotel', 'A', '073083647183', 1, REF(a) FROM addresses WHERE a.street='67 St. Michaels Road', 'Single room with shared bathroom with access to Joining rooms'
-facility_varry_type(
-    facility_type('Room services', 'Workers clean everyones room when they are not in their rooms', 0, '09.00', '08.00', 0.00),
-    facility_type('Restraunt', 'Access food and drinks with family and friends', 150, '09.00', '23.30', 0.00),
-    facility_type('Spa', 'Access to a nice spa with your significant other', 120, '12.00','21.30', 0.00)
-));
+INSERT INTO hotels (hotel_id, name, rating, contact_no, capacity, description, facilities, addresses)
+SELECT 300005, 'Premier Hotel', 'A', '073083647183', 1 , 'Single room with shared bathroom with access to Joining rooms',
+facilities_varray_type(
+    facilities_type('Room services', 'Workers clean everyones room when they are not in their rooms', 0, '09:00', '08:00', 0.00),
+    facilities_type('Restraunt', 'Access food and drinks with family and friends', 150, '09:00', '23:30', 0.00),
+    facilities_type('Spa', 'Access to a nice spa with your significant other', 120, '12:00','21:30', 0.00)), 
+    REF(a) FROM addresses a WHERE street='67 St. Michaels Road';
+
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MON/YYYY';
 
 -- TRIPS
 
