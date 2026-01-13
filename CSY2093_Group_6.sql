8e688c029543d02c661663de8d1da1ae41340e66
Yameen <sm1.yameen@gmail.com>
2026-01-13 15:30:56 +0000

    13/01/2026, what ive got, ready to work type shii

--- CSY2093_Group_6.sql
+++ CSY2093_Group_6.sql
@@ -189,7 +189,7 @@ VALUES (100004, 'WARREN', 'BROWNE', '24-APR-2003', address_type('4 SILENT ROAD',
 INSERT INTO travellers
 VALUES (100005, 'HUGO', 'VEIL', '06-JAN-1800', address_type('13 SERRIN LAND', 'UTOPIA', 'SERITH'));
 
--- TRIP CATEGORIES                                                                      --The Date still included the year despite doing what caroll said, ask her again
+-- TRIP CATEGORIES (JUNYO WIP)
 
 ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MON';
 
@@ -315,7 +315,7 @@ INSERT INTO hotels (hotel_id, name, rating, contact_no, capacity, description, f
 SELECT 300005, 'PREMIER HOTEL', 'A', '073083647183', 1 , 'SINGLE ROOM WITH SHARED BATHROOM WITH ACCESS TO JOINING ROOMS',
 facilities_varray_type(
     facilities_type('ROOM SERVICES', 'WORKERS CLEAN EVERYONE''S ROOM WHEN THEY ARE NOT IN THEIR ROOMS', 0, '09:00', '08:00', 0.00),
-    facilities_type('RESTAURANT', 'ACCESS FOOD AND DRINKS WITH FAMILY AND FRIENDS', 150, '09:00', '23:30', 0.00),
+    facilities_type('RESTRAUNT', 'ACCESS FOOD AND DRINKS WITH FAMILY AND FRIENDS', 150, '09:00', '23:30', 0.00),
     facilities_type('SPA', 'ACCESS TO A NICE SPA WITH YOUR SIGNIFICANT OTHER', 120, '12:00','21:30', 0.00)), 
     REF(a) FROM addresses a WHERE street='67 ST. MICHAELS ROAD';
 
@@ -329,7 +329,7 @@ VALUES (400001, 200001, 300001, 'ALPINE BREAK',
     '17-JUN-2025', '25-AUG-2025',
     activity_table_type(
         activity_type('MOUNTAIN HIKE', 'GUIDED GROUP HIKE THROUGH SCENIC MOUNTAIN TRAILS', 12, 4, 20, 'OUTDOOR'),
-        activity_type('LAKESIDE PICNIC', 'RELAXING PICNIC BY THE LAKE WITH PROVIDED MEALS', 8, 15, 30, 'CULINARY'),
+        activity_type('LAKESIDE PICNIC', 'RELAXING PICNIC BY THE LAKE WITH PROVIDED MEALS', 20, 8, 15, 'CULINARY'),
         activity_type('ROCK CLIMBING SESSION', 'BEGINNER-FRIENDLY CLIMBING ON NATURAL SURFACES', 3, 3, 12, 'ADVENTURE'))
 );
 
@@ -373,40 +373,31 @@ VALUES (400005, 200005, 300005, 'ISLAND RESORT RETREAT',
         activity_type('SUNSET BOAT RIDE', 'EVENING BOAT RIDE WITH SCENIC OCEAN VIEWS', 1, 1, 30, 'LEISURE'))
 );
 
--- TICKETS
+-- TICKETS (UPDATE THIS YAM !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! )
 
 INSERT INTO tickets (ticket_id, trip_id, traveller_id, name, price, description)
 VALUES (500001, 400001, 100001, 'CHILD', 200.00, 'THIS TICKET IS ONLY VALID FOR CUSTOMERS AGED 0-17');
 
 INSERT INTO tickets (ticket_id, trip_id, traveller_id, name, price, description)
-VALUES (500002, 400002, 100002, 'ADULT', 500.00, 'THIS TICKET IS ONLY VALID FOR CUSTOMERS OVER THE AGE OF 17');
+VALUES (500002, 400001, 100002, 'ADULT', 500.00, 'THIS TICKET IS ONLY VALID FOR CUSTOMERS OVER THE AGE OF 17');
 
 INSERT INTO tickets (ticket_id, trip_id, traveller_id, name, price, description)
-VALUES (500003, 400003, 100003, 'STUDENT', 300.00, 'THIS TICKET IS ONLY VALID FOR CUSTOMERS WHO ARE CURRENTLY STUDENTS');
+VALUES (500003, 400002, 100003, 'STUDENT', 300.00, 'THIS TICKET IS ONLY VALID FOR CUSTOMERS WHO ARE CURRENTLY STUDENTS');
 
 INSERT INTO tickets (ticket_id, trip_id, traveller_id, name, price, description)
-VALUES (500004, 400004, 100004, 'DISABLED', 250.00, 'THIS TICKET IS ONLY VALID FOR CUSTOMERS WHO HAVE DISABILITIES');
+VALUES (500004, 400002, 100004, 'DISABLED', 250.00, 'THIS TICKET IS ONLY VALID FOR CUSTOMERS WHO HAVE DISABILITIES');
 
 INSERT INTO tickets (ticket_id, trip_id, traveller_id, name, price, description)
-VALUES (500005, 400005, 100005, 'CARER', 0.00, 'THIS TICKET IS ONLY VALID FOR CUSTOMERS WHO CARE FOR A PERSON WITH A DISABILITY');
+VALUES (500005, 400002, 100005, 'CARER', 0.00, 'THIS TICKET IS ONLY VALID FOR CUSTOMERS WHO CARE FOR A PERSON WITH A DISABILITY');
 
--- -- QUERIES                   THESE ARE NOT COMPLETELY FORMAT YET!!! MOST OF THEM LOOK QUITE BAD :(
+-- -- QUERIES
 
--- Simple one
-
--- The duration look really weird(it does not show which is the start/finish), it is perfectly fine for developer but not for user
--- maybe this problem can be fix with function/procedure part
-
--- (yam comment):                       ASK CAROLE ABOUT THIS 'HOW TO QUERY SIMPLE VARRAYS AND DISPLAY THE INFORMATION IN A NICELY FORMATTED WAY'
-
-COLUMN name FORMAT a15;
-COLUMN duration FORMAT a46;
-
+-- SIMPLE QUERY
 SELECT trip_category_id, name, minimum_age
 FROM trip_categories;
 
 -- OBJECT REFERENCED IN TABLES
-COLUMN name FORMAT a18;                 -- (yam comment): see how the formatting is for this, i think this is more legible
+COLUMN name FORMAT a18;
 COLUMN street FORMAT a15;
 COLUMN city FORMAT a15;
 COLUMN country FORMAT a15;
@@ -423,8 +414,6 @@ SELECT t.traveller_id, t.firstname, t.address.city CITY, t.address.country COUNT
 FROM travellers t;
 
 -- VARRAY
--- THE OPENING/CLOSING TIME IS A DATE INSTEAD OF TIME       unresolved - yam dont know dis unfortunately
--- Formatting doesn't work here for some reason             (yam comment): fixed by using the pseudoname instead of the column name, check if u want
 COLUMN hotel_name FORMAT a15;
 COLUMN facility_name FORMAT a15;
 
@@ -433,7 +422,6 @@ FROM hotels h, TABLE(h.facilities) f
 WHERE hotel_id = 300005;
 
 -- QUERYING TABLES WITH NESTED TABLES
--- Formatting doesn't work here for some reason             (yam comment): fixed formatting; refer to line 427 comment
 COLUMN trip_name FORMAT a11;
 COLUMN activity_name FORMAT a16;
 COLUMN genre FORMAT a13;
@@ -448,7 +436,7 @@ SELECT t.activities
 FROM trips t
 WHERE t.trip_id = 400001) a;
 
--- QUERYING VARRAY USING ALIAS AND AGGREGATE FUNCTION
+-- QUERYING VARRAY USING ALIAS AND AGGREGATE FUNCTION (Querying Simple VArray)
 SELECT tc.trip_category_id,
        tc.name,
        tc.minimum_age,
@@ -472,7 +460,7 @@ SELECT traveller_id, firstname, surname FROM travellers WHERE dob > '31-MAY-2005
 MINUS
 SELECT traveller_id, firstname, surname FROM travellers WHERE dob > '31-MAY-2003';
 
--- AGGREGATE FUNCTIONS
+-- AGGREGATE FUNCTIONS  -- double check yam
 
 -- This show the amount of travellers
 SELECT COUNT(traveller_id)
@@ -539,15 +527,12 @@ LEFT JOIN travellers tr
 
 -- SUB-QUERIES : Find the information of Travellers who have a Student Ticket
 
-COLUMN firstname FORMAT a15;
-COLUMN surname FORMAT a7;
-
 SELECT tr.traveller_id, tr.firstname, tr.surname
 FROM travellers tr
 WHERE (tr.traveller_id) IN (
-    SELECT ti.traveller_id
+    SELECT ti.traveller_id, ti.name
     FROM tickets ti
-    WHERE name = 'STUDENT'
+    WHERE name = 'STUDENTS'
 );
 
 /*
@@ -555,4 +540,29 @@ NOTES
 
 EXCEPTION HANDLING
 
+Things we gotta fix / do
+
+TDLR:
+@everyone
+
+- optional : exception handling (way later down the line)
+
+- get pl sql done:
+
+- Warren also doing switch cases and loops (he basically doing):
+    - proceudres (Warren)
+    - functions (Warren)
+    - cursors (Warren)
+
+Yam:
+- doing checks
+- still do the testing table shenanigans once pl sql is sorted
+
+Junyo:
+- fixing dates n stuff
+
+Nat:
+- do a procedure that has a trigger involved with it
+
 */
+
